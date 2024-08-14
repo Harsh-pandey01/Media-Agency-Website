d36ea70189427374671acef103b7b9e3a83eb5e5
Harsh-Pandey <harshcse7571@gmail.com>
2024-08-14 11:38:26 -0700

    Done Services

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const headerSection = document.querySelector('.header-section')
 const scrollTopBtn = document.querySelector('.scroll-top-btn')
+const hamburgurBtn = document.querySelector('.hamburgur')
+const closeHamburgurBtn = document.querySelector('.close-hamburgur-btn')
+const navbar = document.querySelector('nav')
+
+
 
 window.addEventListener('scroll',(e)=>{
     if(window.scrollY > 100){
@@ -16,3 +21,18 @@ window.addEventListener('scroll',(e)=>{
 scrollTopBtn.addEventListener('click',(e)=>{
     window.scrollTo({ top: 0, behavior: 'smooth' })
 })
+
+// Navbar toggle functionalities
+hamburgurBtn.addEventListener('click',(e)=>{
+    hamburgurBtn.classList.add('close')
+    navbar.classList.add('open')
+    closeHamburgurBtn.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+})
+
+closeHamburgurBtn.addEventListener('click',(e)=>{
+    hamburgurBtn.classList.remove('close')
+    navbar.classList.remove('open')
+    document.body.style.overflow = 'auto'
+})
+
